91ad3ad628211493c5418c9c7916d174b415dd0b
Sebardo <sebastian.sasturain@gmail.com>
2018-01-23 21:15:09 +0100

    Add half amount on pdf bis 11

--- lib/util.class.php
+++ lib/util.class.php
@@ -697,7 +697,7 @@ class util extends sfActions
                     $b = sfContext::getInstance()->getI18N()->__('registration.trans230-b', array('%importe%' => $importe));
                     $c = sfContext::getInstance()->getI18N()->__('registration.trans230-c', array('%date%' => $date->format('d/m/Y')));
                     $txt = $a .' €' .  $b .' €' .  $c;
-                    $pdf->Cell(0, 0, $txt, 0, 0, 'L', 0, '', 0, false, 'M', 'C');
+                    $pdf->Cell(30, 0, $service->getPrice() . ' €', array('B' => array('dash' => 1, 'width' => 0)), 0, 'R', 0, '', 0, false, 'T', 'T');
                 }
             }
             else {
